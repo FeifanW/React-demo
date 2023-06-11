d1791bee07a6b2b5f0a2bf03b58552922e9e63a2
FeifanW <qihang791@163.com>
2023-06-12 05:28:50 +0800

    练习hook

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './assets/images/logo.svg';
 import styles from './App.module.css';
 // import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
+import RobotDiscount from './components/RobotDiscount';
 import ShoppingCart from './components/ShoppingCart';
 
 interface Props {
@@ -60,9 +61,17 @@ const App: React.FC<Props> = (props) => {
       {!error || (error !== '' && <div>网站出错：{error}</div>)}
       {!loading ? (
         <div className={styles.robotList}>
-          {robotGallery.map((r) => (
-            <Robot id={r.id} email={r.email} name={r.name}></Robot>
-          ))}
+          {robotGallery.map((r, index) =>
+            index % 2 === 0 ? (
+              <RobotDiscount
+                id={r.id}
+                email={r.email}
+                name={r.name}
+              ></RobotDiscount>
+            ) : (
+              <Robot id={r.id} email={r.email} name={r.name}></Robot>
+            )
+          )}
         </div>
       ) : (
         <h2>loading 加载中</h2>
--- my-app-ts/src/components/RobotDiscount.tsx
+++ my-app-ts/src/components/RobotDiscount.tsx
@@ -1,20 +1,21 @@
 import React, { useContext } from 'react';
 import styles from './Robot.module.css';
 import { appContext } from '../AppState';
-import { withAddToCart } from './AddToCart';
+import { useAddToCart } from './AddToCart';
 
-export interface RobotProps {
+interface RobotProps {
   id: number;
   name: string;
   email: string;
-  addToCart: (id, name) => void;
 }
 
-const Robot: React.FC<RobotProps> = ({ id, name, email, addToCart }) => {
+const RobotDiscount: React.FC<RobotProps> = ({ id, name, email }) => {
   const value = useContext(appContext);
+  const addToCart = useAddToCart();
   return (
     <div className={styles.cardContainer}>
       <img alt="robot" src={`https://robohash.org/${id}`}></img>
+      <h2>打折商品</h2>
       <h2>{name}</h2>
       <p>{email}</p>
       <p>作者：{value.username}</p>
@@ -23,4 +24,4 @@ const Robot: React.FC<RobotProps> = ({ id, name, email, addToCart }) => {
   );
 };
 
-export default withAddToCart(Robot);
+export default RobotDiscount;
